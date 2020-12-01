8c54fc320a4de92d46acbcd33acd46a60fdfa76c
Harish Kumar <Harish.behl@gmail.com>
2020-12-01 23:09:07 +0000

    Working code from UI to DB

--- src/main/java/com/CSV/Processing/CSVProcessing/controllers/MyCsvController.java
+++ src/main/java/com/CSV/Processing/CSVProcessing/controllers/MyCsvController.java
@@ -37,6 +37,8 @@ public class MyCsvController {
                 csvService.save(file);
 
                 message = "Uploaded the file successfully" + file.getOriginalFilename();
+                return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+
             } catch (Exception e) {
                 message = "Could not upload the file successfully" + file.getOriginalFilename();
                 e.printStackTrace();
