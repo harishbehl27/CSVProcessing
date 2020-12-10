364d9205204d0be44b77af2386c16da074f49703
Harish Kumar <Harish.behl@gmail.com>
2020-12-10 19:19:58 +0000

    removed wanted files

--- src/main/java/com/CSV/Processing/CSVProcessing/controllers/MyCsvController.java
+++ src/main/java/com/CSV/Processing/CSVProcessing/controllers/MyCsvController.java
@@ -32,10 +32,10 @@ public class MyCsvController {
 
 
     @PostMapping("/upload")
-    public ResponseEntity<ResponseMessage> uploadCsvData(@RequestParam(value = "file") MultipartFile file) {
+    public ResponseEntity<ResponseMessage> uploadCsvData(@RequestParam(value = "file") MultipartFile[] files) {
 
         String message = "";
-
+        for (final MultipartFile file : files) {
             if (CSVHelper.hasCSVFormat(file)) {
                 try {
                     csvService.save(file);
@@ -43,24 +43,28 @@ public class MyCsvController {
                     message = "Uploaded the file successfully" + file.getOriginalFilename();
 
                     logger.info(message);
-                    return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
 
-                } catch (Exception e) {
+
+                }
+
+                catch (Exception e) {
                     message = "Could not upload the file successfully" + file.getOriginalFilename();
                     logger.error("Error" + e);
-                    return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+
                 }
 
 
+            } else {
+
+                message = "Please upload a valid csv file ";
+                logger.error("Error" + message);
+            }
+
 
         }
 
-
-            message = "Please upload a valid csv file ";
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
-
-
-        }
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+    }
 
 
 
--- src/main/java/com/CSV/Processing/CSVProcessing/service/CsvService.java
+++ src/main/java/com/CSV/Processing/CSVProcessing/service/CsvService.java
@@ -6,6 +6,8 @@ import com.CSV.Processing.CSVProcessing.repository.CityRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,67 +18,36 @@ import java.util.concurrent.CompletableFuture;
 
 
 @Service
-public class CsvService implements ICsvService{
-
-    @Autowired
-    private CityRepository cityRepository;
+public class CsvService implements ICsvService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CsvService.class);
-
-
-    ///###Approach with Aync threads######
-
-//    public List<City> save(MultipartFile file) throws Exception {
-//        final long start = System.currentTimeMillis();
-//
-//        try {
-//            List<City> cities= CSVHelper.csvToCities(file.getInputStream());
-//
-//            LOGGER.info("Saving a list of cars of size {} records", cities.size() + "file name is" + file.getOriginalFilename());
-//            cityRepository.saveAll(cities);
-//
-//            LOGGER.info("Elapsed time: {}", (System.currentTimeMillis() - start));
-//            return cities;
-//
-//        } catch (IOException e) {
-//
-//            LOGGER.error("Failed to parse CSV file {}", e);
-//            throw new Exception("Failed to parse CSV file {}", e);
-//        }
-//
-//
-//    }
+    @Autowired
+    private CityRepository cityRepository;
 
     @Async
     public CompletableFuture<List<City>> save(MultipartFile file) throws Exception {
         final long start = System.currentTimeMillis();
 
-        try {
-            List<City> cities= CSVHelper.csvToCities(file.getInputStream());
+            try {
 
-            LOGGER.info("Saving a list of cars of size {} records", cities.size() + "file name is" + file.getOriginalFilename());
-            cityRepository.saveAll(cities);
+                List<City> cities = CSVHelper.csvToCities(file.getInputStream());
 
-            LOGGER.info("Elapsed time: {}", (System.currentTimeMillis() - start));
-            return CompletableFuture.completedFuture(cities);
+                LOGGER.info("Saving a list of cars of size {} records", cities.size() + "file name is" + file.getOriginalFilename());
+                cityRepository.saveAll(cities);
 
-        } catch (IOException e) {
+                LOGGER.info("Elapsed time: {}", (System.currentTimeMillis() - start));
+                return CompletableFuture.completedFuture(cities);
 
-            LOGGER.error("Failed to parse CSV file {}" + file.getOriginalFilename(), e);
-            throw new Exception("Failed to parse CSV file {}" + file.getOriginalFilename(), e);
-        }
+            } catch (IOException e) {
+
+                LOGGER.error("Failed to parse CSV file {}" + file.getOriginalFilename(), e);
+                throw new Exception("Failed to parse CSV file {}" + file.getOriginalFilename(), e);
+            }
+
+
 
 
     }
 
-    @Async
-    public CompletableFuture<List<City>> getAllCars() {
-
-        LOGGER.info("Request to get a list of cars");
-        final List<City> cities = cityRepository.findAll();
-        return CompletableFuture.completedFuture(cities);
-    }
 
 }
-
-
